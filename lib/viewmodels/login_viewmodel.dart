5ea9b6050b6eeddb9fc3704fbb64ecb3fb744d2a
ReKerFrost <nmendez@utem.cl>
2024-07-10 23:57:06 -0400

    Actualizacion de vista de Perfil e integracion de un historial de login funcional

--- lib/viewmodels/login_viewmodel.dart
+++ lib/viewmodels/login_viewmodel.dart
@@ -1,7 +1,9 @@
 import 'package:logger/logger.dart';
 import 'package:lumotareas/models/user.dart';
 import 'package:lumotareas/services/user_service.dart';
-import 'package:flutter/material.dart'; // Importante para usar Navigator
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart'; // Importar intl para formatear fechas y horas
 
 class LoginViewModel extends ChangeNotifier {
   final UserService _userService = UserService();
@@ -26,9 +28,33 @@ class LoginViewModel extends ChangeNotifier {
     return message;
   }
 
-  // Método para iniciar sesión con Google SignIn
+  Future<void> _addLoginHistory() async {
+    final prefs = await SharedPreferences.getInstance();
+    final now = DateTime.now().toLocal(); // Asegúrate de usar la hora local
+    final loginRecord = now.toIso8601String();
+
+    List<String>? loginHistory = prefs.getStringList('loginHistory');
+    if (loginHistory == null) {
+      loginHistory = [];
+    }
+
+    loginHistory.add(loginRecord);
+    await prefs.setStringList('loginHistory', loginHistory);
+  }
+
+  Future<List<Map<String, String>>> getLoginHistory() async {
+    final prefs = await SharedPreferences.getInstance();
+    final loginHistory = prefs.getStringList('loginHistory') ?? [];
+
+    return loginHistory.map((record) {
+      final dateTime = DateTime.parse(record).toLocal();
+      final date = DateFormat('dd/MM/yyyy').format(dateTime);
+      final time = DateFormat('HH:mm').format(dateTime);
+      return {'date': date, 'time': time};
+    }).toList();
+  }
+
   Future<void> signInWithGoogle(BuildContext context) async {
-    // Pushea una ventana de carga
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return const Scaffold(
         body: Center(
@@ -49,11 +75,11 @@ class LoginViewModel extends ChangeNotifier {
       Usuario? user = await _userService.signInWithGoogle();
       if (user != null) {
         _currentUser = user;
+        await _addLoginHistory(); // Registrar la hora de inicio de sesión
         notifyListeners();
         _logger.d(
             'Inicio de sesión exitoso con correo y contraseña: ${user.email}');
         setMessage('Inicio de sesión exitoso con correo y contraseña');
-        // Navegar a '/main' y reemplazar todas las rutas anteriores
         if (context.mounted) {
           Navigator.pushNamedAndRemoveUntil(context, '/main', (route) => false);
         }
@@ -71,11 +97,9 @@ class LoginViewModel extends ChangeNotifier {
     }
   }
 
-  // Método para iniciar sesión con email y contraseña
   Future<void> signInWithEmailAndPassword(
       BuildContext context, String email, String password) async {
     try {
-      // Pushea una ventana de carga
       Navigator.push(context, MaterialPageRoute(builder: (context) {
         return const Scaffold(
           body: Center(
@@ -96,11 +120,11 @@ class LoginViewModel extends ChangeNotifier {
           await _userService.signInWithEmailAndPassword(email, password);
       if (user != null) {
         _currentUser = user;
+        await _addLoginHistory(); // Registrar la hora de inicio de sesión
         notifyListeners();
         _logger.d(
             'Inicio de sesión exitoso con correo y contraseña: ${user.email}');
         setMessage('Inicio de sesión exitoso con correo y contraseña');
-        // Navegar a '/main' y reemplazar todas las rutas anteriores
         if (context.mounted) {
           Navigator.pushNamedAndRemoveUntil(context, '/main', (route) => false);
         }
@@ -117,9 +141,7 @@ class LoginViewModel extends ChangeNotifier {
     }
   }
 
-  // Método para cerrar sesión
   Future<void> signOut(BuildContext context) async {
-    // Pushea una ventana de carga
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return const Scaffold(
         body: Center(
@@ -136,14 +158,12 @@ class LoginViewModel extends ChangeNotifier {
       );
     }));
 
-    // 500ms de retraso para mostrar la ventana de carga
     await Future.delayed(const Duration(milliseconds: 500));
     try {
       await _userService.signOut();
       _currentUser = null;
       _logger.d('Sesión cerrada correctamente');
       setMessage('Sesión cerrada correctamente');
-      // navegar a '/login' y reemplazar todas las rutas anteriores
       if (context.mounted) {
         Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
       }
@@ -151,11 +171,13 @@ class LoginViewModel extends ChangeNotifier {
     } catch (e) {
       _logger.e('Error al cerrar sesión: $e');
       setMessage('Error al cerrar sesión');
+      if (context.mounted) {
+        Navigator.pop(context); // Cerrar la ventana de carga si hay un error
+      }
     }
   }
 
   Future<Usuario?> registerUserWithEmailAndPassword(
-      // TODO: falta pantalla de carga?
       Map<String, dynamic> formData) async {
     _logger.d('Datos del formulario desde LoginViewModel: $formData');
 
