d78ddc541a416884891915212a4c90f12cae3542
moisesnks <mleiva@utem.cl>
2024-07-12 02:52:33 -0400

    enhancement: mejora en la UI del login_screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:logger/logger.dart';
+import 'package:lumotareas/widgets/contenedor_widget.dart';
 import 'package:provider/provider.dart';
 import 'package:lumotareas/viewmodels/login_viewmodel.dart';
 import 'package:lumotareas/widgets/welcome_title.dart';
+import './register_form.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -25,67 +27,113 @@ class LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
-            child: Consumer<LoginViewModel>(
-              builder: (context, viewModel, child) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    renderTitle2(),
-                    const SizedBox(height: 40),
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
+          child: Consumer<LoginViewModel>(
+            builder: (context, viewModel, child) {
+              return Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/no-org.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: SingleChildScrollView(
+                  child: Contenedor(
+                    height: MediaQuery.of(context).size.height,
+                    padding:
+                        const EdgeInsets.only(left: 16, right: 16, top: 16),
+                    direction: Axis.vertical,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      renderTitle2(),
+                      Contenedor(
+                        height: MediaQuery.of(context).size.height * 0.8,
+                        padding: const EdgeInsets.only(
+                            top: 100, left: 16, right: 16, bottom: 16),
+                        direction: Axis.vertical,
+                        mainAxisAlignment: MainAxisAlignment.start,
                         crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          const Text(
-                            'Inicio de sesión',
-                            style: TextStyle(
-                              fontFamily: 'Lexend',
-                              fontSize: 32,
-                              color: Colors.white,
-                            ),
-                          ),
-                          Column(
+                          Contenedor(
+                            height: MediaQuery.of(context).size.height * 0.5,
+                            direction: Axis.vertical,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             crossAxisAlignment: CrossAxisAlignment.stretch,
                             children: [
-                              ElevatedButton.icon(
-                                onPressed: () {
-                                  _logger.d('Iniciando sesión con Google...');
-                                  viewModel.signInWithGoogle(context);
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 16),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(16),
+                              Contenedor(
+                                direction: Axis.vertical,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  SizedBox(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.1),
+                                  const Text(
+                                    'Inicia sesión con tu cuenta de Google',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
                                   ),
-                                  backgroundColor:
-                                      const Color.fromARGB(255, 207, 207, 207),
-                                ),
-                                icon: SvgPicture.asset(
-                                  'assets/images/google-svgrepo.svg',
-                                  width: 24,
-                                  height: 24,
-                                ),
-                                label: const Text(
-                                  'Continuar con Google',
-                                  style: TextStyle(
-                                    color: Colors.black,
+                                  const SizedBox(height: 20),
+                                  ElevatedButton.icon(
+                                    onPressed: () {
+                                      _logger
+                                          .d('Iniciando sesión con Google...');
+                                      viewModel.signInWithGoogle(context);
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 16),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(16),
+                                      ),
+                                      backgroundColor: const Color.fromARGB(
+                                          255, 207, 207, 207),
+                                    ),
+                                    icon: SvgPicture.asset(
+                                      'assets/images/google-svgrepo.svg',
+                                      width: 24,
+                                      height: 24,
+                                    ),
+                                    label: const Text(
+                                      'Continuar con Google',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                      ),
+                                    ),
                                   ),
-                                ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  TextButton(
+                                    onPressed: () {
+                                      _logger.d('Registrarme presionado');
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              RegisterFormWidget(),
+                                        ),
+                                      );
+                                    },
+                                    child: const Text(
+                                      '¿No tienes una cuenta? Regístrate',
+                                    ),
+                                  ),
+                                ],
                               ),
                             ],
+                            // Registrarme es un textbutton que lleva a la pantalla de registro
                           ),
                         ],
                       ),
-                    ),
-                  ],
-                );
-              },
-            ),
+                    ],
+                  ),
+                ),
+              );
+            },
           ),
         ),
       ),
